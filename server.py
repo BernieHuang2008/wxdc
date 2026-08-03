98262a900c2d702abfcf361588b7fde9ed7ca0c9
BernieHuang2008 <berniehuang2008@163.com>
2026-08-03 18:07:05 +0800

    test?

--- server.py
+++ server.py
@@ -476,4 +476,4 @@ def api_register():
         return jsonify({"success": False, "message": "Failed to send registration email."}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=APP_PORT)
+    app.run("0.0.0.0", debug=True, port=APP_PORT)
